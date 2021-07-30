8902f79a88b92a6174c1058bf528d1c21e351b47
Gumercindo-Guilhermina <86901592+Gumercindo-Guilhermina@users.noreply.github.com>
2021-07-29 21:56:52 -0400

    flutter

--- repositorio_teste/lib/pront.dart
+++ repositorio_teste/lib/pront.dart
@@ -17,63 +17,91 @@ class _ProntState extends State<Pront> {
       appBar: AppBar(
         title: Text("Formulário"),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: "Nome"),
-                onSaved: (value) {
-                  nome = value;
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Campo Obrigatório";
-                  }
-                },
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 900,
+              width: MediaQuery.of(context).size.width,
+              child: Form(
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Nome"),
+                        onSaved: (value) {
+                          nome = value;
+                        },
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Campo Obrigatório";
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Sobrenome"),
+                        onSaved: (value) {
+                          sobrenome = value;
+                        },
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Campo Obrigatório";
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        decoration:
+                            InputDecoration(labelText: "Data de Nascimento"),
+                        keyboardType: TextInputType.datetime,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Casa"),
+                        keyboardType: TextInputType.number,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Etnia"),
+                        keyboardType: TextInputType.text,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Sexo"),
+                      ),
+                      TextFormField(
+                        decoration:
+                            InputDecoration(labelText: "Aldeia de Residência"),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Altura"),
+                        keyboardType: TextInputType.number,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Município"),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Polo Base"),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Nome da Mãe"),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "Nome do Pai"),
+                      ),
+                      TextFormField(
+                        decoration:
+                            InputDecoration(labelText: "N° do cartão do SUS"),
+                        keyboardType: TextInputType.number,
+                      ),
+                      TextFormField(
+                        decoration:
+                            InputDecoration(labelText: "N° do cartão do CPF"),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Sobrenome"),
-                onSaved: (value) {
-                  sobrenome = value;
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Campo Obrigatório";
-                  }
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Data de Nascimento"),
-                keyboardType: TextInputType.datetime,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Casa"),
-                keyboardType: TextInputType.number,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Etnia"),
-                keyboardType: TextInputType.text,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Sexo"),
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Aldeia de Residência"),
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Altura"),
-                keyboardType: TextInputType.number,
-              ),
-              RaisedButton(
-                  child: Text(""),
-                  onPressed: () {
-                    // _showSegundaPag(context);
-                  })
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
